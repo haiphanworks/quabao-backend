0da4d2aa88243dd4fbb0b1161eb510ead02fac02
Hải Phan <hai@haiphan.design>
2021-10-13 15:11:20 +0700

    My first commit

---
+++ config/database.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+	defaultConnection: 'default',
+	connections: {
+		default: {
+			connector: 'bookshelf',
+			settings: {
+				client: 'mysql',
+				host: env('DATABASE_HOST', '103.153.73.48'),
+				port: env.int('DATABASE_PORT', 3306),
+				database: env('DATABASE_NAME', 'quabao'),
+				username: env('DATABASE_USERNAME', 'quabao'),
+				password: env('DATABASE_PASSWORD', 'M4tkh4uqu4b40!!!@@@'),
+				ssl: env.bool('DATABASE_SSL', false),
+			},
+			options: {},
+		},
+	},
+})
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'af3f4578-3c9e-4869-b315-00fefef93e36'
+};
