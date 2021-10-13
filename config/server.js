c4475ec816bc46b3150b58f2749a6708eb5f82fb
Hải Phan <hai@haiphan.design>
2021-10-13 16:01:57 +0700

    SSL error

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '169428a01b30f0beb67760166315dad5'),
-    },
-  },
-});
+	host: env('HOST', '0.0.0.0'),
+	port: env.int('PORT', 1337),
+	admin: {
+		auth: {
+			secret: env('ADMIN_JWT_SECRET', '169428a01b30f0beb67760166315dad5'),
+		},
+	},
+})
